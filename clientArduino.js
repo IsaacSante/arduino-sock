1a54d6d0de1c5837c6337ae7e67f498282cd9b7b
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2021-04-04 16:44:49 -0400

    commit two ultrasonic

--- clientArduino.js
+++ clientArduino.js
@@ -5,27 +5,28 @@ var up = true;
 var value = 60;
 var increment = 1;
 var ceiling = 100;
-var c1, c2, c3; 
+var c1, c2, c3, c4; 
 
 function PerformCalc() {
-  if (up == true && value <= ceiling) {
-    value += increment
-
-    if (value == ceiling) {
-      up = false;
-    }
-  } else {
-      up = false
-      value -= increment;
-
-      if (value == 60) {
-        up = true;
+    if (up == true && value <= ceiling) {
+      value += increment
+      
+      if (value == ceiling) {
+        up = false;
       }
-  }
+    } else {
+        up = false
+        value -= increment;
 
-c1 = 'Client One' + value
-c2 = 'Client Two' + (value + 10)
-c3 = 'Distance' + (value/2)
+        if (value == 60) {
+          up = true;
+        }
+    }
+
+      c1 = 'Client One' + value
+      c2 = 'Client Two' + (value + 10)
+      c3 = 'DistanceOne' + (value/2)
+      c4 = 'DistanceTwo' + (value/4)
 
 }
 setInterval(PerformCalc, 1000);
@@ -37,6 +38,7 @@ ws.on('open', function() {
         ws.send(c1)
         ws.send(c2)
         ws.send(c3)
+        ws.send(c4)
       }, 1000)
 })
 
